ed37d2eff47a956ee78f5c46d1cc4a0be4a1ad5e
Eric <ericyeonpark@gmail.com>
2021-04-22 09:51:54 -0700

    getting ready for development

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,12 +1,13 @@
-'''SQLAlchemy User and Tweet models for our database'''
+"""SQLAlchemy User and Tweet models for out database"""
 from flask_sqlalchemy import SQLAlchemy
 
 # creates a DB Object from SQLAlchemy class
 DB = SQLAlchemy()
 
+
 # Making a User table using SQLAlchemy
 class User(DB.Model):
-    '''Creates a User Table with SQLAlchemy'''
+    """Creates a User Table with SQlAlchemy"""
     # id column
     id = DB.Column(DB.BigInteger, primary_key=True)
     # name column
@@ -15,25 +16,25 @@ class User(DB.Model):
     newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
-        return '<User: {}>'.format(self.name)
+        return "<User: {}>".format(self.name)
 
 
 class Tweet(DB.Model):
-    '''Keeps track of tweets for each user'''
+    """Keeps track of Tweets for each user"""
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(300)) # allows for text and links
+    text = DB.Column(DB.Unicode(300))  # allows for text and links
     vect = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
-        'user.id'), nullable = False)
+        'user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
         return "<Tweet: {}>".format(self.text)
 
 
-CREATE_USER_TABLE_SQL = '''
-    CREATE TABLE IS NOT EXIST user(
-        id INT PRIMARY,
-        name STRING NOT NULL
-    )
-'''
+CREATE_USER_TABLE_SQL = """
+  CREATE TABLE IF NOT EXIST user (
+    id INT PRIMARY,
+    name STRING NOT NULL
+  );
+"""
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,13 +1,12 @@
-'''Getting tweets and users from the Twitter DB'''
+"""Getting tweets and users from the Twitter DB"""
 from os import getenv
 import tweepy
 import spacy
 from .models import User, DB, Tweet
 
-
 TWITTER_AUTH = tweepy.OAuthHandler(
-    getenv('TWITTER_API_KEY'), 
-    getenv('TWITTER_API_KEY_SECRET')
+    getenv("TWITTER_API_KEY"),
+    getenv("TWITTER_API_KEY_SECRET")
 )
 TWITTER = tweepy.API(TWITTER_AUTH)
 
@@ -21,39 +20,40 @@ def vectorize_tweet(tweet_text):
 
 
 def add_or_update_user(username):
-    '''
+    """
     Gets twitter user and tweets from twitter DB
-    Gets user by 'username' parameter.
-    '''
+
+    Gets user by "username" parameter.
+    """
     try:
         # gets back twitter user object
         twitter_user = TWITTER.get_user(username)
         # Either updates or adds user to our DB
         db_user = (User.query.get(twitter_user.id)) or User(
             id=twitter_user.id, name=username)
-        DB.session.add(db_user) # Adds user if don't exist
+        DB.session.add(db_user)  # Add user if don't exist
 
-        # Grabbing tweets from 'twitter_user'
+        # Grabbing tweets from "twitter_user"
         tweets = twitter_user.timeline(
             count=200,
             exclude_replies=True,
-            incude_rts=False,
-            tweet_mode='Extended',
+            include_rts=False,
+            tweet_mode="extended",
             since_id=db_user.newest_tweet_id
         )
 
         if tweets:
             db_user.newest_tweet_id = tweets[0].id
 
-        #tweets is a list of tweet objects
+        # tweets is a list of tweet objects
         for tweet in tweets:
             # type(tweet) == object
             tweet_vector = vectorize_tweet(tweet.full_text)
             db_tweet = Tweet(
-                id = tweet.id,
-                text = tweet.full_text,
-                vect = tweet_vector
-                )
+                id=tweet.id,
+                text=tweet.full_text,
+                vect=tweet_vector
+            )
             db_user.tweets.append(db_tweet)
             DB.session.add(db_tweet)
 
